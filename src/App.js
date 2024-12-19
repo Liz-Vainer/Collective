a786dd43d5d3604e02a8e89bd51e7635c9e7cf86
maria <mariaba@ac.sce.ac.il>
2024-12-20 00:13:52 +0200

    new branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Home from './Components/Homepage/Home'; 
+<<<<<<< HEAD
+=======
+import SettingsPage from './Components/SettingsPage/SettingsPage'
+>>>>>>> 7f5af59 (Add files to maria branch)
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginSignup />} />
         <Route path="/home" element={<Home />} />
+<<<<<<< HEAD
+=======
+        <Route path="/settings" element={<SettingsPage/>}/>
+>>>>>>> 7f5af59 (Add files to maria branch)
       </Routes>
     </Router>
   );

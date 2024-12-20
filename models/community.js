d00f896c9a2c86ce028ddc11792fc965ff360f08
Shon <sean.matatov@gmail.com>
2024-12-20 10:28:28 +0200

    everything works

--- models/community.js
+++ models/community.js
@@ -4,6 +4,8 @@ const communitySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   category: { type: String, required: true, unique: false },
   population: { type: Number, required: false },
+  lan: { type: Number, required: true, unique: true },
+  lat: { type: Number, required: true, unique: true },
 });
 
 // const Community = mongoose.model("Community", communitySchema);

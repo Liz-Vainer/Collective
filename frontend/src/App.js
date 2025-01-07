d94b5e3872d1ca7b7465f1857299b1f67c0411ab
Shon <sean.matatov@gmail.com>
2025-01-07 14:22:33 +0200

    chat dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,19 @@ import Home from "./Components/Homepage/Home";
 import SettingsPage from "./Components/SettingsPage/SettingsPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./Components/UserContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <UserProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<LoginSignup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<LoginSignup />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </AuthContextProvider>
       </Router>
     </UserProvider>
   );

677693771f3db5665fe41c13cb59b771972db5a9
maria <mariaba@ac.sce.ac.il>
2025-01-22 13:08:18 +0200

    join event test

--- backend/UnitTest/signup.test.js
+++ backend/UnitTest/signup.test.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 import app from "../index.js"; // Your Express app
 import User from "../models/user.js"; // User model for testing
 import mongoose from "mongoose";
-const { ObjectId } = mongoose.Types;
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -37,12 +36,43 @@ describe("POST /add to fav", function () {
       .request(app)
       .post("/add_to_fav")
       .send({
-        id: ObjectId('6787a809637aa9c21113129f'),
+        id: ObjectId(''),
         community: "Park Y.A",
       });
 
-    expect(response).to.have.status(200);
+    expect(re6787a809637aa9c21113129fsponse).to.have.status(200);
     expect(response.body.message).to.equal("Community added to favorites");
     expect(response.body.favorites).to.include("Park Y.A");
+  });
+});
+describe("POST /joinEvent", function () {
+  this.timeout(10000); // Set timeout to 10 seconds for all tests in this block
+
+  let eventId;
+  let userId;
+  
+  it("should successfully add a user to the event", async () => {
+    const response = await chai.request(app)
+      .post("/joinEvent")
+      .send({
+        EventId: eventId,
+        userId: userId,
+      });
+
+    expect(response).to.have.status(200);
+    expect(response.body.message).to.equal("User successfully added to the Event");
+    expect(response.body.participants).to.include(userId.toString());
+  });
+
+  it("should return 404 if the event does not exist", async () => {
+    const response = await chai.request(app)
+      .post("/joinEvent")
+      .send({
+        EventId: mongoose.Types.ObjectId(), // Invalid event ID
+        userId: userId,
+      });
+
+    expect(response).to.have.status(404);
+    expect(response.body.message).to.equal("Event not found");
   });
 });
